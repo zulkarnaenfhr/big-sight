bad0a165e325d059e062701237e5e19e911b6a66
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-04-26 09:42:22 +0700

    landing page big sight responsive tablet

--- src/container/BigSight-LandingPage/BigSight-LandingPage-Section5/BigSight-LandingPage-Section5.jsx
+++ src/container/BigSight-LandingPage/BigSight-LandingPage-Section5/BigSight-LandingPage-Section5.jsx
@@ -11,7 +11,7 @@ class BigsightLandingpageSection5 extends Component {
                             product achievement
                         </h1>
                         <div className="row">
-                            <div className="col-4 BigSight-LandingPage-Section5-Content-Container-Section2">
+                            <div className="col-4 BigSight-LandingPage-Section5-Content-Container-Left BigSight-LandingPage-Section5-Content-Container-Section2">
                                 <div>
                                     <img loading="lazy" className="BigSight-LandingPage-Section5-Content-CostSaving-Logo" src={require("../../../src/Big-Sight/Landing Page/Section-5-Left.png")} alt="" border="0"></img>
                                     <div>
@@ -37,14 +37,14 @@ class BigsightLandingpageSection5 extends Component {
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-4 BigSight-LandingPage-Section5-Content-Container-Section2">
+                            <div className="col-4 BigSight-LandingPage-Section5-Content-Container-Right BigSight-LandingPage-Section5-Content-Container-Section2">
                                 <div>
                                     <img loading="lazy" className="BigSight-LandingPage-Section5-Content-CostSaving3-Logo" src={require("../../../src/Big-Sight/Landing Page/Section-5-Right.png")} alt="" border="0"></img>{" "}
                                     <div className="div3">
-                                        <p className="BigSight-LandingPage-Section5-Content-CostSaving1-Title" data-aos="fade-up" data-aos-offset="100">
+                                        <p className="BigSight-LandingPage-Section5-Content-CostSaving1-Title" data-aos="fade-up" data-aos-offset="0">
                                             Cost-saving practices
                                         </p>
-                                        <h3 className="BigSight-LandingPage-Section5-Content-CostSaving1-Desc" data-aos="fade-up" data-aos-offset="100">
+                                        <h3 className="BigSight-LandingPage-Section5-Content-CostSaving1-Desc" data-aos="fade-up" data-aos-offset="0">
                                             Meningkatkan efisiensi biaya operasional untuk kebutuhan bisnis anda.
                                         </h3>
                                     </div>
